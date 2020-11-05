d0c251489ce1403bfdb2a586a50c7acb3e2bbdf4
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-11-05 10:02:50 +0530

    App

--- src/Must-Have/ShowCard.js
+++ src/Must-Have/ShowCard.js
@@ -10,12 +10,18 @@ const ShowCard = ({ value, id }) => {
     setState(ql);
   };
 
+  const showModal1 = () =>{
+    setShow(true);
+  }
+
   return (
     <>
       <div className="card">
         {value.map(({ ql, pt, tp, fp }) => (
           <div className="inCard" key={Math.random() + 1}>
-            <div style={{ backgroundColor: "#eeff" }}>Qualified {ql}</div>
+            <div style={{ color: "#ffffff", backgroundColor: "#5a6f90" }}>
+              Qualified {ql}
+            </div>
             <br />
             <div style={{ fontWeight: "bold" }}>{pt}</div>
             <p>Per Qualified Lead</p>
@@ -26,7 +32,13 @@ const ShowCard = ({ value, id }) => {
             <p>Platform Fee Per Month</p>
             <div style={{ fontWeight: "bold" }}>{tp}</div>
             <br />
-            <div style={{ backgroundColor: "#eeff", fontWeight: "bold" }}>
+            <div
+              style={{
+                color: "#ffffff",
+                backgroundColor: "#5a6f90",
+                fontWeight: "bold"
+              }}
+            >
               {fp}
             </div>
             <br />
@@ -35,6 +47,15 @@ const ShowCard = ({ value, id }) => {
           </div>
         ))}
         {show && <Modal setShow={setShow} state={state} />}
+        <div className="inCard">
+        <div style={{ color: "#ffffff", backgroundColor: "#5a6f90" }}>
+          Enterprise
+        </div>
+        <br />
+        <p style={{marginTop:"75px", marginBottom:"75px"}}>Want more than 80 qualified leads per month?</p>
+        <br/>
+        <button onClick={showModal1}>Get in touch</button>
+      </div>
       </div>
     </>
   );
